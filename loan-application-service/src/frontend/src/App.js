9e025e81bc704ae26cb57d3c01e0b9fcf6d23c25
Betül Şahin <betulsahin3387@hotmail.com>
2021-10-06 13:53:18 +0300

    Create frontend

---
+++ loan-application-service/src/frontend/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+
+Typescript =
+Javascripti tip güvenli hale getirmek için
+microsoft taraftından geliştirildi. Extra özelliklerde
+eklendi. Üst seviye bir dil.
+
+React, angular, vue.. =
+Single page applicationdır.
+
+Redux = state management.
+state bir datanın son hali.
+
+*/
