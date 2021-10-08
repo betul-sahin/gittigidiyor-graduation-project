976a778725066c059326dfdf516a67ee4335ac6e
Betül Şahin <betulsahin3387@hotmail.com>
2021-10-08 10:39:13 +0300

    Add ui structure - layouts, pages

--- loan-application-service/src/frontend/src/App.js
+++ loan-application-service/src/frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import Dashboard from './layouts/Dashboard';
+import { Container } from 'semantic-ui-react';
+import Navi from './layouts/Navi';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navi />
+      <Container className="main">
+        <Dashboard />
+      </Container>
     </div>
   );
 }
@@ -25,7 +18,6 @@ function App() {
 export default App;
 
 /*
-
 Typescript =
 Javascripti tip güvenli hale getirmek için
 microsoft taraftından geliştirildi. Extra özelliklerde
@@ -37,4 +29,20 @@ Single page applicationdır.
 Redux = state management.
 state bir datanın son hali.
 
+rest -> geriye kalan parametreler demek
+function(id, ...products){
+}
+
+spread -> ayrıştırmak anlamında.
+rest te array geçilmişse ayrıştırmak
+icin kullanıyoruz.
+let points = [1, 2 ,3, 4 ,5]
+console.log(...points)
+
+destructuring ->
+let populations = [1000, 2000, 3000]
+let [small, medium, large] = populations
+console.log(small)
+console.log(medium)
+console.log(large)
 */
