8aa00e1dbfa971c0478e6bbf471e89664c670e74
Betül Şahin <betulsahin3387@hotmail.com>
2021-10-10 12:29:41 +0300

    Create new apply for a loan

--- loan-application-service/src/frontend/src/index.js
+++ loan-application-service/src/frontend/src/index.js
@@ -4,16 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-import {Provider} from "react-redux"
-import { configureStore } from './store/configureStore';
+//import {Provider} from "react-redux"
+//import { configureStore } from './store/configureStore';
 
-const store = configureStore()
+//const store = configureStore()
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
--- loan-application-service/src/frontend/src/pages/LoanAdd.jsx
+++ loan-application-service/src/frontend/src/pages/LoanAdd.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 import * as Yup from "yup";
 import { Formik, Form,Field, ErrorMessage } from "formik";
 import { FormField, Button, Label, Container } from "semantic-ui-react";
+import axios from 'axios';
 
 export default function LoanAdd() {
-    const initialValues = { customerId: 1, creditAmount: 10, creditLimitMultiplier:4, creditResult:"APPROVAL"};
+    const initialValues = { creditLimitMultiplier:4, identificationNumber:"11111111111"};
 
     // dogrulama yapmamizi saglayan bir yapi
     const schema = Yup.object({
-        customerId: Yup.number().required("Zorunlu"),
-        creditAmount: Yup.number().required("Zorunlu"),
         creditLimitMultiplier: Yup.number().required("Zorunlu"),
-        creditResult: Yup.string().required("Zorunlu")
+        identificationNumber: Yup.string().required("Zorunlu")
     })
+
+    const postData = (loan) => {
+        console.log(loan)
+        axios.post(`http://localhost:8080/api/v1/loans/`, loan)
+    }
 
     return (
         <Container>
@@ -20,23 +24,24 @@ export default function LoanAdd() {
                 initialValues={initialValues} 
                 validationSchema={schema}
                 onSubmit = {(values)=>{
-                    console.log(values)
+                    console.log("formik")
+                    postData(values)
                 }}
                 >
                 <Form className="ui form">
                     <FormField>
-                        <Field name="customerId" placeholder="customer Id"></Field>
-                        <ErrorMessage name="customerId" render={error => 
+                        <Field name="identificationNumber" placeholder="identificationNumber Id"></Field>
+                        <ErrorMessage name="identificationNumber" render={error => 
                             <Label pointing basic color="red" content={error}></Label>
                         }></ErrorMessage>
                     </FormField>
                     <FormField>
-                        <Field name="creditAmount" placeholder="credit Amount"></Field>
-                        <ErrorMessage name="creditAmount" render={error => 
+                        <Field name="creditLimitMultiplier" placeholder="creditLimitMultiplier Amount"></Field>
+                        <ErrorMessage name="creditLimitMultiplier" render={error => 
                             <Label pointing basic color="red" content={error}></Label>
                         }></ErrorMessage>
                     </FormField>
-                    <Button color="green" type="submit">Ekle</Button>
+                    <Button color="green" type="submit">Apply Now</Button>
                 </Form>
             </Formik>
         </Container>
