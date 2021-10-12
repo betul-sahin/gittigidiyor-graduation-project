6723baae8c53a500bd3f2effc95319b3180e5178
Betül Şahin <betulsahin3387@hotmail.com>
2021-10-12 16:26:46 +0300

    Optimize loan apply form

--- loan-application-service/src/frontend/src/pages/LoanAdd.jsx
+++ loan-application-service/src/frontend/src/pages/LoanAdd.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import * as Yup from "yup";
 import { Formik, Form,Field, ErrorMessage } from "formik";
-import { FormField, Button, Label, Container } from "semantic-ui-react";
+import { FormField, FormGroup, FormCheckbox, Button, Label, Container } from "semantic-ui-react";
 import axios from 'axios';
 
 export default function LoanAdd() {
-    const initialValues = { creditLimitMultiplier:4, identificationNumber:"11111111111"};
+     const initialValues = { identificationNumber:"",
+    firstName:"", lastName:"", phoneNumber:"", monthlyIncome:""};
 
     // dogrulama yapmamizi saglayan bir yapi
     const schema = Yup.object({
-        creditLimitMultiplier: Yup.number().required("Zorunlu"),
-        identificationNumber: Yup.string().required("Zorunlu")
+        identificationNumber: Yup.string().required("Zorunlu"),
+        firstName: Yup.string().required("Zorunlu"),
+        lastName: Yup.string().required("Zorunlu"),
+        phoneNumber: Yup.string().required("Zorunlu"),
+        monthlyIncome: Yup.number().required("Zorunlu")
     })
 
     const postData = (loan) => {
@@ -29,19 +33,48 @@ export default function LoanAdd() {
                 }}
                 >
                 <Form className="ui form">
-                    <FormField>
-                        <Field name="identificationNumber" placeholder="identificationNumber Id"></Field>
-                        <ErrorMessage name="identificationNumber" render={error => 
-                            <Label pointing basic color="red" content={error}></Label>
-                        }></ErrorMessage>
-                    </FormField>
-                    <FormField>
-                        <Field name="creditLimitMultiplier" placeholder="creditLimitMultiplier Amount"></Field>
-                        <ErrorMessage name="creditLimitMultiplier" render={error => 
-                            <Label pointing basic color="red" content={error}></Label>
-                        }></ErrorMessage>
-                    </FormField>
-                    <Button color="green" type="submit">Apply Now</Button>
+                    <FormGroup widths={2}>
+                        <FormField>
+                            <Field name="identificationNumber" placeholder="TC Identification Number"></Field>
+                            <ErrorMessage name="identificationNumber" render={error => 
+                                <Label pointing basic color="red" content={error}/>
+                            }></ErrorMessage>
+                        </FormField>
+                    </FormGroup>
+                    <FormGroup widths={3}>
+                        <FormField>
+                            <Field name="firstName" placeholder="First Name"></Field>
+                            <ErrorMessage name="firstName" render={error =>
+                                <Label pointing basic color="red" content={error}/>
+                            }></ErrorMessage>
+                        </FormField>
+                        <FormField>
+                            <Field name="lastName" placeholder="Last Name"></Field>
+                            <ErrorMessage name="lastName" render={error =>
+                                <Label pointing basic color="red" content={error}/>
+                            }></ErrorMessage>
+                        </FormField>
+                    </FormGroup>
+                    <FormGroup widths={3}>
+                        <FormField>
+                            <Field name="phoneNumber" placeholder="Phone Number"></Field>
+                            <ErrorMessage name="phoneNumber" render={error => 
+                                <Label pointing basic color="red" content={error}/>
+                            }></ErrorMessage>
+                        </FormField>
+                        <FormField>
+                            <Field name="monthlyIncome" placeholder="Monthly Income"></Field>
+                            <ErrorMessage name="monthlyIncome" render={error => 
+                                <Label pointing basic color="red" content={error}></Label>
+                            }></ErrorMessage>
+                        </FormField>
+                    </FormGroup>
+                    <FormGroup>
+                        <FormCheckbox label='I accept the processing of my personal data within the scope specified in the Information.' checked={true} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Button color="green" type="submit">Apply Now</Button>
+                    </FormGroup>
                 </Form>
             </Formik>
         </Container>
